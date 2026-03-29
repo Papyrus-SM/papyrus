41d6762e632112167e0d2b1a0bc840f36958afcd
João Reis <j.victor.reis20044@gmail.com>
2026-03-29 16:37:14 -0300

    ignore criado, repositório organizado

---
+++ backend/src/config/conexao_exemplo.php
@@ -0,0 +1,16 @@
+<?php
+// este é um arquivo de exemplo.
+// Crie um arquivo 'conexao.php' e preencha suas credenciais locais.
+
+$host = 'localhost';
+$db   = 'nome_do_seu_banco';
+$user = 'usuario';
+$pass = 'senha';
+$port = "3306";
+
+try {
+    $pdo = new PDO("mysql:host=$host;port=$port;dbname=$db;charset=utf8", $user, $pass);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Erro na conexão: " . $e->getMessage());
+}
